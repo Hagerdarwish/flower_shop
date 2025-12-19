83c3518d5b14f56bf24b811caa2020cbd600efdb
Hager Darwish <107562928+Hagerdarwish@users.noreply.github.com>
2025-12-19 22:09:00 +0200

    add endpoint constants

---
+++ lib/app/core/values/app_endpoint_strings.dart
@@ -0,0 +1,14 @@
+class AppEndpointString{
+  static const String baseUrl='https://exam.elevateegy.com/api/v1/';
+  static const String sendEmail='/auth/forgotPassword';
+  static const String resetCode='/auth/verifyResetCode';
+  static const String changePassword='/auth/changePassword';
+  static const String getSubject='/subjects';
+  static const String getExam ='/exams';
+
+
+
+
+
+
+}
