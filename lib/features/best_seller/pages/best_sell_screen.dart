35e4d83c1c8ffd3398975b45f2ec80a74b5bb207
Rahma Ashraf <rahmaashraf.dev@gmail.com>
2026-01-03 10:39:42 +0200

    Add search bar to Best Seller screen

--- lib/features/best_seller/pages/best_sell_screen.dart
+++ lib/features/best_seller/pages/best_sell_screen.dart
@@ -1,12 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flower_shop/app/config/base_state/base_state.dart';
 import 'package:flower_shop/features/best_seller/menager/best_seller_intent.dart';
+import 'package:flower_shop/features/home/presentation/widgets/search_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flower_shop/features/best_seller/best_seller_card/best_seller_card.dart';
 import 'package:flower_shop/features/best_seller/menager/best_sell_cubit.dart';
 import 'package:flower_shop/features/best_seller/menager/best_sell_state.dart';
 import 'package:flower_shop/generated/locale_keys.g.dart';
+
 class BestSellerScreen extends StatefulWidget {
   const BestSellerScreen({super.key});
 
@@ -21,11 +23,14 @@ class _BestSellerScreenState extends State<BestSellerScreen> {
     // Trigger the load
     context.read<BestSellerCubit>().doIntent(LoadBestSellersEvent());
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Best seller')),
+      appBar: AppBar(
+        titleSpacing: 0,centerTitle: true,
+        elevation: 0,
+        title:  SearchTextField(),
+      ),
       body: BlocBuilder<BestSellerCubit, BestSellerState>(
         builder: (context, state) {
           final productsResource = state.products;
@@ -41,7 +46,7 @@ class _BestSellerScreenState extends State<BestSellerScreen> {
                 return Center(child: Text(LocaleKeys.no_products_found.tr()));
               }
               return GridView.builder(
-                padding: const EdgeInsets.all(16),
+                padding: const EdgeInsets.all(8),
                 itemCount: items.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
